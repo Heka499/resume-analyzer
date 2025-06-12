011617e9085cc4867c29dc100ce40b080489519d
Heikki Jarvinen <h.e.jarvinen@gmail.com>
2025-06-12 17:00:28 +0300

    feat: enhance ResumeUploader to navigate to resume page on successful upload

--- components/ResumeUploader.tsx
+++ components/ResumeUploader.tsx
@@ -1,15 +1,22 @@
 "use client";
 
 import { UploadButton } from "../utils/uploadthing";
+import { useRouter } from "next/navigation";
 
 export default function ResumeUploader() {
+  const router = useRouter();
+
+  const handleUploadSuccess = async () => {
+    router.push("/resume");
+  };
+
   return (
     <div className="flex flex-col items-center gap-4">
       <UploadButton
         endpoint="resumeUploader"
         onClientUploadComplete={(res) => {
           console.log("Upload complete:", res);
-          alert("Resume uploaded successfully!");
+          handleUploadSuccess();
         }}
         onUploadError={(error) => {
           console.error("Upload error:", error);
